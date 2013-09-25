b9b9aad9fb2a51fb01ee81755eb043b1163f9487
Pavel Podkopajev <ppodkopajev@gmail.com>
2013-09-25 16:47:38 +0300

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-	validates_presence_of :title, :description
+	validates_presence_of :title, :description, :image_url
 	validates_numericality_of :price, greater_than_or_equal_to: 0.01
 	validates_uniqueness_of :title, :message => "must be unique"
 	validates_format_of :image_url, allow_blank: true, :with => %r{\.(gif|jpg|png)\Z}i, :message => "must be a URL for GIF, JPG or PNG image."
