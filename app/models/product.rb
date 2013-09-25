6dd5f6c5e4f92d62235d36ceb723e6cd63354a9e
Pavel Podkopajev <ppodkopajev@gmail.com>
2013-09-25 21:49:19 +0300

    Add model relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates_presence_of :title, :description, :image_url
 	validates_length_of :title, minimum: 10, too_short: 'please enter at least 10 characters'
 	validates_numericality_of :price, greater_than_or_equal_to: 0.01, message: 'must be greater than or equal to 0.01'
@@ -8,4 +12,15 @@ class Product < ActiveRecord::Base
 	def self.latest
 		Product.order(:updated_at).last
 	end
+
+	private
+
+		def ensure_not_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, 'Line Items present')
+				return false
+			end
+		end
 end
